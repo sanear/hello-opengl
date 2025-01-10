65eae36913cb27691c1ec831e7f01c358195e9b7
claire <claire@claires-MacBook-Pro.local>
2025-01-10 01:27:40 -0700

    Add a little math

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int doMath(int x, int y) {
+  int result = 1;
+  for (int i = 0; i < y; i++) {
+    result *= x;
+  }
+  return result;
+}
+
 int main() {
-  cout << "Hello, World!" << endl;
+  int base = 2;
+  int exp = 10;
+
+  printf("%i**%i = %i\n", base, exp, doMath(base, exp));
   return 0;
 }

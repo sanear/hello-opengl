e5c2a5a25891a36b50a35cd63f00d53735093a97
claire <claire@claires-MacBook-Pro.local>
2025-01-28 00:45:26 -0700

    First pass at triplicating draw logic; broken asf

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -26,8 +26,8 @@ void logShaderProgramError(GLuint shaderProgram);
 static const char *vertex_shader_text =
     "#version 330 core\n"
     "uniform mat4 MVP;\n"
-    "in vec3 vCol;\n"
     "in vec2 vPos;\n"
+    "in vec3 vCol;\n"
     "out vec3 color;\n"
     "void main()\n"
     "{\n"
@@ -45,17 +45,15 @@ static const char *fragment_shader_text = "#version 330 core\n"
 
 static struct InputState inputState = {false, false, false, false, 0.0f};
 static Triangle triangle = Triangle();
-
-static const Vertex centerTriangle[3] = {{{-0.6f, -0.4f}, {1.f, 0.f, 0.f}},
-                                         {{0.6f, -0.4f}, {0.f, 1.f, 0.f}},
-                                         {{0.f, 0.6f}, {0.f, 0.f, 1.f}}};
-
-void setupVertexBuffer(GLuint *vertex_buffer) {
-  glGenBuffers(1, vertex_buffer);
-  glBindBuffer(GL_ARRAY_BUFFER, *vertex_buffer);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(centerTriangle), centerTriangle,
-               GL_STATIC_DRAW);
-}
+static const Vertex triangles[3][3] = {{{{-0.3f, 0.f}, {1.f, 0.f, 0.f}},
+                                        {{0.3f, 0.f}, {0.f, 1.f, 0.f}},
+                                        {{0.f, 0.6f}, {0.f, 0.f, 1.f}}},
+                                       {{{-0.6f, -0.4f}, {1.f, 0.f, 0.f}},
+                                        {{0.f, 0.4f}, {0.f, 1.f, 0.f}},
+                                        {{-0.3f, 0.f}, {0.f, 0.f, 1.f}}},
+                                       {{{0.f, -0.4f}, {1.f, 0.f, 0.f}},
+                                        {{0.6f, -0.4f}, {0.f, 1.f, 0.f}},
+                                        {{0.3f, 0.f}, {0.f, 0.f, 1.f}}}};
 
 void setupShader(GLuint *shader, const char *shaderText) {
   glShaderSource(*shader, 1, &shaderText, NULL);
@@ -122,20 +120,35 @@ GLFWwindow *initGlfwWindow() {
   return window;
 }
 
+// TODO: What if we just do each of these two processes once for each triangle..?
+void setupVertexBuffer(GLuint *vertex_buffer, const Vertex* triangle) {
+  glGenBuffers(1, vertex_buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, *vertex_buffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex), triangle, GL_STATIC_DRAW);
+}
+
+// TODO: Do we need to do this entire process for each triangle...
+// Does this mean that the shaders have to be rewritten knowing exactly how many to draw???
+// Surely they're a per-triangle configuration...
 void setupVertexArrayObject(const GLuint *program, GLuint *vertex_array,
                             GLint *mvp_location) {
-  // mvp = model*view*projection, a matrix multiplication for camera/3d world
-  // stuff vPos = vector of positions vCol = vector of colors
+  // Get the location of MVP based on the vertex shader code
   *mvp_location = glGetUniformLocation(*program, "MVP");
+  // Get the location of vpos and vcol based on the vertex shader code
   const GLint vpos_location = glGetAttribLocation(*program, "vPos");
   const GLint vcol_location = glGetAttribLocation(*program, "vCol");
 
+  // TODO: all of the below seems to be predicated on the drawing of one triangle
+  // Generate vertex array object
   glGenVertexArrays(1, vertex_array);
+  // Bind vertex array object to context or whatever
   glBindVertexArray(*vertex_array);
+  // Enable the attribute stored at vpos_location (i.e., vpos); same for vcol
   glEnableVertexAttribArray(vpos_location);
+  glEnableVertexAttribArray(vcol_location);
+  // This is grabbing the first triangle by position... but where are the other two???
   glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex),
                         (void *)offsetof(Vertex, pos));
-  glEnableVertexAttribArray(vcol_location);
   glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
                         (void *)offsetof(Vertex, col));
 }
@@ -153,9 +166,6 @@ void doEverything() {
   cout << "Initializing window..." << endl;
   GLFWwindow *window = initGlfwWindow();
 
-  GLuint vertex_buffer;
-  setupVertexBuffer(&vertex_buffer);
-
   cout << "Initializing shaders..." << endl;
   GLuint vertex_shader, fragment_shader;
   setupVertexShader(&vertex_shader);
@@ -166,11 +176,16 @@ void doEverything() {
   GLuint program;
   setupShaderProgram(&program, shaders, 2);
 
-  GLuint vertex_array;
-  GLint mvp_location;
-  cout << "Setting up vertex array object..." << endl;
-  setupVertexArrayObject(&program, &vertex_array, &mvp_location);
+  cout << "Setting up vertex buffer and array objects..." << endl;
+  GLuint vertex_arrays[3];
+  GLint mvp_locations[3];
+  for (int i = 0; i < 3; i++) {
+    GLuint vertex_buffer;
+    const Vertex *t = triangles[i]; // TODO: I don't understand why I can't use array type here
+    setupVertexBuffer(&vertex_buffer, t);
 
+    setupVertexArrayObject(&program, &vertex_arrays[i], &mvp_locations[i]);
+  }
   // Main loop!
   // TODO: this should live separate from the graphics right?
   cout << "Starting render loop..." << endl;
@@ -190,9 +205,11 @@ void doEverything() {
                    ratio, triangle.elapsedPaused);
 
     glUseProgram(program);
-    glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat *)&mvp);
-    glBindVertexArray(vertex_array);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    for (int i = 0; i < 3; i++) {
+      glUniformMatrix4fv(mvp_locations[i], 1, GL_FALSE, (const GLfloat *)&mvp);
+      glBindVertexArray(vertex_arrays[i]);
+      glDrawArrays(GL_TRIANGLES, 0, 3);
+    }
 
     glfwSwapBuffers(window);
 

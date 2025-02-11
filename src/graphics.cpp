b78ef3e3b6ccbf31663be9691841d020500db338
Claire S <claire@sannier.us>
2025-02-11 00:55:06 -0700

    Comments

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -34,7 +34,10 @@ static const char *vertex_shader_text =
     "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
     "    color = vCol;\n"
     "}\n";
+// TODO: What does color do here that isn't done in the fragment shader?
 
+
+// TODO: Add input parameter "noise" that varies the color by some magnitude
 static const char *fragment_shader_text = "#version 330 core\n"
                                           "in vec3 color;\n"
                                           "out vec4 fragment;\n"
